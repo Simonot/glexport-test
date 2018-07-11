96cfdf1cc3bdfe989691dfc27c83da2fb0e23d8b
Simon Duvergier <duvergiersimon@yahoo.fr>
2018-07-11 20:03:39 +0200

    Controllers:Shipments: add sort and filter.

--- app/controllers/api/v1/shipments_controller.rb
+++ app/controllers/api/v1/shipments_controller.rb
@@ -2,6 +2,7 @@ class Api::V1::ShipmentsController < Api::V1::ApplicationController
   include BasicErrorRendering
 
   before_action :find_company_and_shipments
+  before_action :sort_filter_params
 
   def index
   end
@@ -17,5 +18,16 @@ class Api::V1::ShipmentsController < Api::V1::ApplicationController
       end
     rescue ActiveRecord::RecordNotFound
       not_found! 'Company', params[:company_id]
+    end
+
+    def sort_filter_params
+      if params[:international_transportation_mode]
+        @shipments = @shipments.where(international_transportation_mode: params[:international_transportation_mode])
+      end
+      if params[:sort]
+        invalid_params!('sort', params[:sort]) and return unless params[:sort] == 'international_departure_date'
+        invalid_params!('direction', params[:direction]) and return unless ['asc', 'desc'].include?(params[:direction])
+        @shipments = @shipments.order(params[:sort].to_sym => params[:direction].to_sym)
+      end
     end
 end
